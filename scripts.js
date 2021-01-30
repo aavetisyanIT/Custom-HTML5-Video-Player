609e93a4a88edde006332fc9293f2c2695b53139
aavetisyanit <aavetisyanit@gmail.com>
2021-01-29 20:20:09 -0600

    Create progress bar, style and functionality

--- scripts.js
+++ scripts.js
@@ -4,8 +4,8 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButton = player.querySelector('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 //functionality
 function togglePlay() {
@@ -21,8 +21,27 @@ function updateButton() {
 	toggle.textContent = this.paused ? '►' : '❚ ❚';
 }
 
+//skip forward and back
+function skip() {
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+//range update on volume and playback speed
+function handleRangeUpdate() {
+	video[this.name] = this.value;
+}
+
+//progress
+function handleProgress() {
+	const precent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${precent}%`;
+}
+
 //Event listeners
 toggle.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
